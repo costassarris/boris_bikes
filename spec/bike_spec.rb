47bbfd6a68e6b816b09dc010014087954b9d506b
Costas Sarris <costassarris@gmail.com>
2015-02-03 18:39:26 +0000

    refactored

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -1,24 +1,23 @@
 #link to the Bike class
-require 'bike'
+require './lib/bike'
 
 #We're describing the functionality of a specific class, in this case 'Bike'
 describe Bike do
+
+  let(:bike) {Bike.new}
 	#this is a specific feature (behaviour) that we expect to be present
 	it 'should not be broken after we create it' do
-		the_bike = Bike.new #initialise any object of Bike class
 		#expect an instance of this class
 		#a method "broken?" that should return false
-		expect(the_bike).not_to be_broken
+		expect(bike).not_to be_broken
 	end
 
   it 'should be able to break' do
-    bike = Bike.new
     bike.break!
     expect(bike).to be_broken
   end
 
   it 'should be able to get fixed' do
-  	bike = Bike.new
   	bike.break!
   	bike.fix!
   	expect(bike).not_to be_broken
